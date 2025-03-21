3ab650c13936097cb1430ba9a6b17777bd1f7704
themichaelusa <michael.usachenko@icloud.com>
2025-03-21 15:18:26 -0700

    updating withrepo

--- withrepo/download.py
+++ withrepo/download.py
@@ -21,15 +21,13 @@ from withrepo.utils import (
     RepoArguments,
     RepoProvider,
     LanguageGroup,
-    get_all_paths_from_root_relative,
-    get_language_from_ext,
-    keep_file_for_language,
+    copy_and_split_root_by_language_group,
 )
 
 # Third party
 import httpx
 
-# constants
+# CONSTANTS
 CHUNK_SIZE = 2 * 1024 * 1024
 
 PROVIDER_TO_URL_MAP = {
@@ -37,54 +35,6 @@ PROVIDER_TO_URL_MAP = {
     RepoProvider.GITLAB: "https://gitlab.com",
     RepoProvider.BITBUCKET: "https://bitbucket.org",
 }
-
-
-def copy_and_split_root_by_language_group(abs_root_path) -> List[LanguageGroup]:
-    abs_paths, _ = get_all_paths_from_root_relative(abs_root_path)
-    languages = set()
-
-    for p in abs_paths:
-        lsp_language, language, is_code = get_language_from_ext(p)
-        if is_code:
-            languages.add(lsp_language)
-    languages = [lang for lang in languages if lang]
-
-    copy_paths = []
-    # copy the root directory into a temporary directory per language
-    for _ in range(len(languages)):
-        tmp_parent_dir = tempfile.mkdtemp(prefix="scope_")
-        shutil.copytree(abs_root_path, tmp_parent_dir, dirs_exist_ok=True)
-        copy_paths.append(tmp_parent_dir)
-
-    for copy_path, language in zip(copy_paths, languages):
-        for root, dirs, files in os.walk(copy_path):
-            for file in files:
-                if keep_file_for_language(root, file, language):
-                    continue
-                else:
-                    # print(f"removing file: {os.path.join(root, file)}")
-                    os.remove(os.path.join(root, file))
-
-    # remove copy_paths that only have directories and no files
-    nonempty_copy_paths = []
-    for copy_path, language in zip(copy_paths, languages):
-        files_set = set()
-        for root, dirs, files in os.walk(copy_path):
-            for file in files:
-                files_set.add(file)
-        if not files_set:
-            # print(f"copy_path: {copy_path} is empty")
-            shutil.rmtree(copy_path)
-            continue
-
-        # TODO: figure out if this screws up with path removal on cleanup
-        child_dirs = os.listdir(copy_path)
-        if len(child_dirs) == 1:
-            copy_path = os.path.join(copy_path, child_dirs[0])
-        nonempty_copy_paths.append(LanguageGroup(language, copy_path))
-
-    return nonempty_copy_paths
-
 
 def download_and_extract_archive(url: str) -> Tuple[str, List[LanguageGroup]]:
     """
--- withrepo/withrepo.py
+++ withrepo/withrepo.py
@@ -11,19 +11,112 @@ from withrepo.download import (
     parse_repo_arguments_into_download_url,
     download_and_extract_archive,
 )
-from withrepo.utils import get_language_from_ext
+from withrepo.utils import (
+    get_language_from_ext,
+    copy_and_split_root_by_language_group
+) 
+
+from withrepo.resources.languages import EXT_TO_LANGUAGE_DATA
+
+
+# THIS IS JUST HERE FOR COMPATIBILITY WITH ADRENALINE PROD
+class File(object):
+    def __init__(
+        self,
+        path,
+        abs_path,
+        content=None,
+        language=None,
+        summary=None,
+        has_ingress=False,
+        change_type=None,
+        db_id=None,
+        **kwargs,
+    ):
+        self.db_id = db_id
+        self.path = path  # Relative to the root directory
+        self.abs_path = abs_path  # Relative to the repository root
+        self.content = content
+        self.change_type = change_type
+        self.summary = summary
+
+        self.file_name = File.get_file_name(path)
+        self.file_extension = File.get_file_extension(path)
+
+        if language:
+            self.language = language
+        elif self.file_extension in EXT_TO_LANGUAGE_DATA:
+            self.language = EXT_TO_LANGUAGE_DATA[self.file_extension]["language_mode"]
+        else:
+            self.language = None
+
+        self.embedding = None
+        self.children = []
+        self.has_ingress = has_ingress
+
+    def __repr__(self):
+        return f"File(path={self.path})"
+
+    @staticmethod
+    def get_file_name(path):
+        if not path:
+            return None
+
+        path_components = path.split("/")
+        file_name = path_components[-1]
+        return file_name
+
+    @staticmethod
+    def get_file_extension(path):
+        if not path:
+            return None
+
+        file_name = File.get_file_name(path)
+        if file_name.lower() == "readme":
+            return ".md"
+
+        return f".{file_name.split('.')[-1]}".lower()
+
+    def to_dict(self):
+        return {
+            "path": self.path,
+            "abs_path": self.abs_path,
+            "content": self.content,
+            "language": self.language,
+            "summary": self.summary,
+            "has_ingress": self.has_ingress,
+        }
+
+    def to_string(self):
+        return f"<FILE_PATH>{self.path}</FILE_PATH>\n<FILE_SUMMARY>\n{self.summary}\n</FILE_SUMMARY>"
 
 
 class RepoFile:
     def __init__(self, abs_path: str, relative_path: str, preload: bool = False):
+        self.file_name: str = os.path.basename(abs_path)
+        self.file_extension: str = os.path.splitext(abs_path)[1]
         self.abs_path: str = abs_path
-        self.relative_path: str = relative_path
+        self.path: str = relative_path
         self._contents: str = None
         if preload:
             self.contents()
         _, language, is_code = get_language_from_ext(abs_path)
         self.language: str = language
         self.is_code: bool = is_code
+        # self.tree_sitter_lang = EXT_TO_TREE_SITTER_LANGUAGE.get(self.ext, None)
+
+    def __len__(self) -> int:
+        return len(self.content.split("\n"))
+
+    def __str__(self) -> str:
+        return f"RepoFile(file_name={self.file_name}, path={self.path}, language={self.language}, is_code={self.is_code}, num_lines={len(self)})"
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self.content.split("\n"))
+
+    @property
+    def content(self) -> str:
+        return self.contents()
 
     def contents(self) -> str:
         try:
@@ -32,22 +125,8 @@ class RepoFile:
                     self._contents = f.read()
             return self._contents
         except Exception:
-            print(f"RepoFile::contents() Error reading file {self.abs_path}")
+            # print(f"RepoFile::contents() Error reading file {self.abs_path}")
             return ""
-
-    def __len__(self) -> int:
-        return len(self.contents().split("\n"))
-
-    def __str__(self) -> str:
-        return f"""RepoFile(
-            relative_path={self.relative_path},
-            language={self.language},
-            is_code={self.is_code},
-            num_lines={len(self)}
-        )"""
-
-    def __iter__(self) -> Iterator[str]:
-        return iter(self.contents().split("\n"))
 
 
 class RepoContext:
@@ -62,6 +141,7 @@ class RepoContext:
         self.commit: str = args.commit
         self.branch: str = args.branch
         self.repo_url: str = args.url
+        self.root_dir: str = args.root_dir
         self.provider: RepoProvider = args.provider
         self.lang_groups: List[LanguageGroup] = lang_groups
         self.languages: List[str] = list(
@@ -98,6 +178,8 @@ class RepoContext:
                 for file in files_list:
                     relpath = os.path.relpath(os.path.join(root, file), self.path)
                     abspath = os.path.abspath(os.path.join(root, file))
+                    if self.root_dir and not relpath.startswith(self.root_dir):
+                        continue
                     files.append(RepoFile(abspath, relpath, preload=store))
             if store:
                 self.files = files
@@ -113,6 +195,8 @@ class RepoContext:
                             os.path.join(root, file), lang_group.path
                         )
                         abspath = os.path.abspath(os.path.join(root, file))
+                        if self.root_dir and not relpath.startswith(self.root_dir):
+                            continue
                         lang_trees[lang_group.language].append(
                             RepoFile(abspath, relpath, preload=store)
                         )
@@ -139,18 +223,33 @@ def repo(
     commit: str = "",
     branch: str = "",
     url: str = "",
+    root_dir: str = "",
+    root_path: str = None,
     provider: RepoProvider = RepoProvider.GITHUB,
     cleanup_callback: bool = False,
+    timeit: bool = False,
     log: bool = False,
 ) -> Iterator[RepoContext]:
-    """
-    TODO
-    """
+
     args = RepoArguments(
-        user=user, repo=repo, commit=commit, branch=branch, url=url, provider=provider
+        user=user,
+        repo=repo,
+        commit=commit,
+        branch=branch,
+        url=url,
+        provider=provider,
+        root_dir=root_dir,
+        root_path=root_path,
     )
-    repo_zip_url = parse_repo_arguments_into_download_url(args)
-    source_directory_path, lang_groups = download_and_extract_archive(repo_zip_url)
+
+    if not root_path:
+        repo_zip_url = parse_repo_arguments_into_download_url(args)
+        source_directory_path, lang_groups = download_and_extract_archive(repo_zip_url)
+    else:
+        repo_zip_url = None
+        lang_groups = copy_and_split_root_by_language_group(root_path)
+        source_directory_path = root_path
+
     repo_ctx = RepoContext(source_directory_path, repo_zip_url, args, lang_groups)
     yield repo_ctx
 
